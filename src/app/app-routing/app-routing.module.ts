ad647f5e54d92a576a6e0bbf445e86cfaf3064b8
Aan3dree <an3dree@gmail.com>
2020-12-13 15:43:59 -0300

    adicionando roteamento

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../component/home/home.component';
+import { ConvenioFiscoComponent } from '../component/convenio-fisco/convenio-fisco.component';
+
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'convenio-fisco', component: ConvenioFiscoComponent }
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AppRoutingModule } from '../app/app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsEpioneComponent } from './components/forms-epione/forms-epione.component';
 import { ConvenioFiscoComponent } from './component/convenio-fisco/convenio-fisco.component';
 import { MenuSuperiorComponent } from './component/menu-superior/menu-superior.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormsEpioneComponent,
     ConvenioFiscoComponent,
-    MenuSuperiorComponent
+    MenuSuperiorComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
